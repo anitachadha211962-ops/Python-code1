4682c5f5b311e77d6e91e22b94759309df74029e
SurajChadha001 <surajchadhamca@gmail.com>
2026-02-26 07:25:32 +0530

    commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print("Suraj")
 print(5+6)
-
+print("er")
 
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello Suraj, {name}!"
 
 if __name__ == "__main__":
     print(greet("Suraj"))
