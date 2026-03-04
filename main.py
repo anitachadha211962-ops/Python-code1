ec3fd327b626d72225a9d12e4782368757ec39ed
SurajChadha001 <surajchadhamca@gmail.com>
2026-03-04 20:08:15 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 print("Suraj")
 print(5+6)
-<<<<<<< HEAD
 print(1)
 print("wwwwwwwwwwwwwwwwwwwwww")
 print("Rebase code")
@@ -8,10 +7,6 @@ print(5+8)
 print(ab)
 
 print(f"Suraj is a good boy {9}")
-=======
-print("er")
-
->>>>>>> 5f70e79ed27c13428e8bfe875e32bd02e494141a
 def greet(name):
     return f"Hello Suraj, {name}!"
 
